50c549ee99d813ac5acece6f2215cefeacaa70d0
UnaiMurilloMesa <unaimurillo123@gmail.com>
2025-04-26 01:08:45 +0200

    Funcionalidad completa

--- src/main/java/org/umm/App.java
+++ src/main/java/org/umm/App.java
@@ -2,6 +2,7 @@ package org.umm;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -33,7 +34,7 @@ public class App
             e.printStackTrace();
         }
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
 
 
@@ -63,6 +64,94 @@ public class App
             System.out.println();
         }
 
+
+
+
+
+
+        // Solucionar el sudoku
+        if (solveSudoku(sudoku)) {
+            System.out.println("Sudoku solved:");
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(sudoku[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("Sudoku not solved");
+        }
+
+
+
+
+
+
+        // Rellenar la tabla
+        Actions actions = new Actions(driver);
+        WebElement firstCell = sudokuSquare.findElement(By.tagName("td"));
+        firstCell.click();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                String num = String.valueOf(sudoku[i][j]);
+                actions.sendKeys(num).perform();
+                actions.sendKeys("\u0009").perform();
+            }
+        }
+        System.out.println("Sudoku completed");
+
+
+
+
         System.out.println("Quitting...");
+    }
+
+    private static boolean solveSudoku(int[][] sudoku) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (sudoku[row][col] == 0) {
+                    for (int k = 1; k <= 9; k++) {
+                        if (isValid(sudoku, row, col, k)) {
+                            sudoku[row][col] = k;
+
+                            if (solveSudoku(sudoku)) {
+                                return true;
+                            }
+
+                            sudoku[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isValid(int[][] sudoku, int row, int col, int k) {
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[row][i] == k) {
+                return false;
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            if (sudoku[i][col] == k) {
+                return false;
+            }
+        }
+
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (sudoku[startRow + i][startCol + j] == k) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
